7947309bbbf1a7b5f4d9876e81472fd7ace83b7c
Norman <normanndot@gmail.com>
2022-09-08 16:24:38 +0530

    Added files to reuse

---
+++ Sources/ServiceHandler/HTTPMethod.swift
@@ -0,0 +1,28 @@
+//
+//  HTTPMethod.swift
+//  Atheletes
+//
+//  Created by Norman, ThankaVijay on 07/09/22.
+//
+
+import Foundation
+
+public enum HTTPMethod {
+    case get
+    case post(Encodable)
+    case put(Encodable)
+    case patch(Encodable)
+    case delete
+}
+
+extension HTTPMethod {
+    var string: String {
+        switch self {
+        case .get: return "get"
+        case .post: return "post"
+        case .put: return "put"
+        case .patch: return "patch"
+        case .delete: return "delete"
+        }
+    }
+}
